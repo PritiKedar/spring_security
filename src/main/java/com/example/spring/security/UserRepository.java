1c487de44227b4934d608a12be49a59b79636682
PritiKedar <pritikedar@gmail.com>
2022-06-14 16:15:13 -0400

    initial commit

---
+++ src/main/java/com/example/spring/security/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.spring.security;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.spring.security.model.User;
+
+//import net.perscholas.springboot.springsecurity.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  User findByEmail(String email);
+}
+
